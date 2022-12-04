1cc6141462112861367f01c8d35bcbb55733d417
corndoghh <corndoghh@github.com>
2022-12-04 17:03:33 +0000

    commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+//default port
+const PORT = 3000
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+app.use("/scripts", express.static(path.join(__dirname, "/src/scripts")))
+app.use("/sounds", express.static(path.join(__dirname, "/src/sounds")))
+app.use("/sprites", express.static(path.join(__dirname, "/src/sprites")))
+
+
+app.get("/", (_req, res) => {
+    res.sendFile(path.join(__dirname, "/index.html"))
+})
